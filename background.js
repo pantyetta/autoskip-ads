c1540cc783a1b7d17bbd73a16071109ee86a53a1
pantyetta <email@pantyetta.com>
2023-09-23 01:24:26 +0900

    add player observer

--- background.js
+++ background.js
@@ -2,22 +2,28 @@ const skipAds = (tabId) => {
     chrome.scripting.executeScript({
         target : {tabId: tabId },
         func: () => {
-            const $skip_container =  document.getElementsByClassName("video-ads ytp-ad-module")[0] || null;
-
-            if($skip_container == null)  return;
-            const observer = new MutationObserver(() => {
-                const $skip_button = document.getElementsByClassName('ytp-ad-skip-button ytp-button')[0] || null;
-                if($skip_button == null)    return;
-                $skip_button.click();
-            });
-            
             const config = { 
                 attributes: true, 
                 childList: true, 
                 characterData: true,
                 subtree: true,
             };
-            observer.observe($skip_container, config);
+
+            const $player = document.getElementById('movie_player');
+            const playerObserver = new MutationObserver(() => {
+                const $skip_container =  document.getElementsByClassName("video-ads ytp-ad-module")[0] || null;
+                if($skip_container == null)  return;
+
+                const observer = new MutationObserver(() => {
+                    const $skip_button = document.getElementsByClassName('ytp-ad-skip-button ytp-button')[0] || null;
+                    if($skip_button == null)    return;
+                    $skip_button.click();
+                });
+                
+                observer.observe($skip_container, config);
+                playerObserver.disconnect();
+            });
+            playerObserver.observe($player, config);
         },
     });
 }
