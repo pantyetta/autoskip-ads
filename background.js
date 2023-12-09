52df4ebd523ad462c24e1994647c92a16ad412e0
pantyetta <email@pantyetta.com>
2023-12-09 13:44:22 +0900

    fix action order conditions

--- background.js
+++ background.js
@@ -10,15 +10,11 @@ const skipAds = (tabId) => {
             };
 
             const $player = document.getElementById('movie_player');
-            const video = document.querySelector('video');
 
             const playerObserver = new MutationObserver(() => {
                 const $skip_container =  document.getElementsByClassName("video-ads ytp-ad-module")[0] || null;
-                if(!$skip_container.children.length)    return;
-
-                if(video.currentTime == video.duration)    return;
-
-                video.currentTime = video.duration;
+                console.log("skip container");
+                if($skip_container == null || !$skip_container.children.length)    return;
 
                 const observer = new MutationObserver(() => {
                     const $skip_button = document.getElementsByClassName('ytp-ad-skip-button-modern ytp-button')[0] || null;
@@ -28,8 +24,14 @@ const skipAds = (tabId) => {
 
                     observer.disconnect();
                 });
-                
                 observer.observe($skip_container, config);
+                
+                const video = document.querySelector('video');
+                try {
+                    video.currentTime = video.duration;
+                    console.log('skip ads');
+                } catch (error) {}
+                
             });
             playerObserver.observe($player, config);
         },
