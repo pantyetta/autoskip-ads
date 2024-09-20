27cadb73e0d0b4acb81eb352a65ae12bef707988
unknown <email@pantyetta.com>
2024-09-20 23:45:38 +0900

    niconico first support beta

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-const skipAds = (tabId) => {
+const y_skipAds = (tabId) => {
     chrome.scripting.executeScript({
         target : {tabId: tabId },
         func: () => {
@@ -35,6 +35,53 @@ const skipAds = (tabId) => {
     });
 }
 
+const n_skipAds = (tabId) => {
+    chrome.scripting.executeScript({
+        target : {tabId: tabId },
+        func: () => {
+            const config = { 
+                attributes: true, 
+                childList: true, 
+                characterData: true,
+                subtree: true,
+            };
+
+            const $root = document.getElementById('root');
+            let skip_enable = true;
+            
+            const rootObserver = new MutationObserver(() => {
+                const video = document.querySelector("#nv_watch_VideoAdContainer > div > div:nth-child(1) > video") || null;
+                
+                if(!video)  return;
+
+                if(!video.duration) {
+                    skip_enable = true;
+                    return;
+                }
+
+                if(video.currentTime == video.duration){
+                    const skipButton = document.querySelector("#nv_watch_VideoAdContainer > button") || null;
+                    if(!skipButton) return;
+                    skipButton.click();
+                    console.log("click")
+                    return;
+                }
+
+                if(!skip_enable) return;
+
+
+                try {
+                    video.currentTime = video.duration;
+                    console.log(video.currentTime)
+                    skip_enable = false;
+                } catch (error) {}
+                
+            });
+            rootObserver.observe($root, config);
+        },
+    });
+}
+
 
 const isEmpty = (obj) => {
     return !Object.keys(obj).length;
@@ -44,15 +91,28 @@ chrome.webNavigation.onCommitted.addListener((details) => {
     if (details.transitionType === 'reload') {
         chrome.storage.session.remove([details.tabId.toString()]);
     }
-}, {url: [{urlMatches : 'https://www.youtube.com/*'}]});
+}, {url: [{urlMatches : 'https://www.youtube.com/*'}, {urlMatches : 'https://www.nicovideo.jp/watch/*'}]});
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    const pattern = /^https:\/\/(www\.)?youtube\.com\/.+/
-    if(tab.url.match(pattern) && changeInfo.status === 'complete'){
+    const y_pattern = /^https:\/\/(www\.)?youtube\.com\/.+/
+    const n_pattern = /^https:\/\/(www\.)?nicovideo\.jp\/watch\/.+/
+
+    if(tab.url.match(y_pattern) && changeInfo.status === 'complete'){
 
         chrome.storage.session.get([tabId.toString()]).then((result) => {
             if(isEmpty(result)){
-                skipAds(tabId);
+                y_skipAds(tabId);
+            }
+        });
+
+        chrome.storage.session.set({ [tabId]: true });
+    }
+
+    if(tab.url.match(n_pattern) && changeInfo.status === 'complete'){
+
+        chrome.storage.session.get([tabId.toString()]).then((result) => {
+            if(isEmpty(result)){
+                n_skipAds(tabId);
             }
         });
 
